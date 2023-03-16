db84c14dc378b78f83b9a0c8f0012c7187fbd723
CedricLittman <cedric@littman.me.uk>
2023-03-16 15:41:15 +0000

    test push of fork

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .forms import ItemForm
+
 
 # Create your tests here.
+
+class TestDjango(TestCase):
+
+    def test_this_thing_works(self):
+        self.assertEqual(1, 1)
+
+    def test_this_thing_works2(self):
+        self.assertEqual(1, 3)
+
+    def test_this_thing_works3(self):
+        self.assertEqual(1, )
+
+    def test_this_thing_works4(self):
+        self.assertEqual(1, 4)
+
+
+
+
+class TestItemForm(TestCase):
+
+    def test_item_form_name_not_empty(self):
+        form = ItemForm({'name' : ''})
+        self.assertFalse(form.is_valid())
+        self.assertFails
